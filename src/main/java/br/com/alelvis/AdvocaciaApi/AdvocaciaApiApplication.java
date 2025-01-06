c75bb9e41971f31f9dc7949028e3542e5951bd14
alelvis3 <alelvis3br@hotmail.com>
2025-01-05 21:10:54 -0300

    Subindo o Spring

---
+++ src/main/java/br/com/alelvis/AdvocaciaApi/AdvocaciaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alelvis.AdvocaciaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvocaciaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvocaciaApiApplication.class, args);
+	}
+
+}
